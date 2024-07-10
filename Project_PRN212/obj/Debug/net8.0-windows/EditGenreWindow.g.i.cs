a1329f3372e3eb607036799ec701925043946b32
Khải Nguyễn <khaihungyt@gmail.com>
2024-07-10 09:51:47 +0700

    Khai v1

--- Project_PRN212/obj/Debug/net8.0-windows/EditGenreWindow.g.i.cs
+++ Project_PRN212/obj/Debug/net8.0-windows/EditGenreWindow.g.i.cs
@@ -54,13 +54,13 @@ namespace Project_PRN212 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.6.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Project_PRN212;component/editgenrewindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Project_PRN212;V1.0.0.0;component/editgenrewindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\EditGenreWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -70,7 +70,7 @@ namespace Project_PRN212 {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.6.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
